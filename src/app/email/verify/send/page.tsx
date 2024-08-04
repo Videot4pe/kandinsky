bf9c098b8212eca1735da67962e93d12e83c2352
Alexandr Kvasnikov <videot4pe@gmail.com>
2024-08-04 10:42:02 +0300

    feat: email verification page

--- src/app/email/verify/send/page.tsx
+++ src/app/email/verify/send/page.tsx
@@ -1,18 +1,31 @@
-import Form from "@/features/auth/ui/email-verification-form";
+import { EmailVerificationForm } from "@/features/auth/ui/email-verification-form";
 
-// Importing the Suspense component from React for asynchronous operations
 import { Suspense } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/shared/ui/card";
 
 export default function Send() {
   return (
-    <div className="flex flex-col">
-      <div className="mb-4">Please verify your email first.</div>
-
-      {/* Using Suspense for asynchronous loading */}
-      <Suspense>
-        {/* Rendering the Form component for email verification initiation */}
-        <Form />
-      </Suspense>
+    <div className="h-[100dvh] w-full flex flex-col justify-center">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="mb-4">Please verify your email first.</div>
+          <Suspense>
+            <EmailVerificationForm />
+          </Suspense>
+        </CardContent>
+      </Card>
     </div>
   );
 }
